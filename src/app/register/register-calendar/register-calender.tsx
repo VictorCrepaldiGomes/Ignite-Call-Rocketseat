c1913f83335301863339b98942aafead75153e6d
Victor Crepaldi <victorcrepaldigomes@gmail.com>
2025-07-11 14:50:47 -0300

    add: new tables in prisma, users, account and sessions

--- src/app/register/register-calendar/register-calender.tsx
+++ src/app/register/register-calendar/register-calender.tsx
@@ -2,9 +2,13 @@
 
 import { Button } from "@/components/ui/button";
 import StepperLayout from "@/components/ui/origin_ui/stepperLayout";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Check } from "lucide-react";
+import { signIn, useSession } from "next-auth/react";
 
 export default function RegisterCalendar() {
+  const session = useSession();
+
+  console.log(session);
   return (
     <div className="flex flex-col items-center justify-center text-justify min-h-screen ">
       <header className="w-full max-w-xl text-center mb-8 rounded-md p-10 border border-emerald-500">
@@ -16,17 +20,23 @@ export default function RegisterCalendar() {
           disponibilidade.
         </p>
         <StepperLayout currentStep={2} />
-          <form className="mt-10 flex flex-col items-center gap-4">
-            <div className="flex items-center justify-between w-full">
-              <label className="text-white  ">Google Calendar</label>
-              <Button variant='outline' className="w-40">
-                Conectar
-              </Button>
-            </div>
-            <Button className="bg-emerald-700 w-full">
-              Próximo passo <ArrowRight />
+        <form className="mt-10 flex flex-col items-center gap-4">
+          <div className="flex items-center justify-between w-full">
+            <label className="text-white  ">Google Calendar</label>
+            <Button
+              variant="outline"
+              className="w-40"
+              type="button"
+              onClick={() => signIn("google")}
+              disabled={session.status === "authenticated"}
+            >
+              {session.status === "authenticated" ? <Check/> : "Conectar"}
             </Button>
-          </form>
+          </div>
+          <Button className="bg-emerald-700 w-full">
+            {session.status === "authenticated" ? "Próximo passo" : "É necessário conectar no Google Calendar"} <ArrowRight />
+          </Button>
+        </form>
       </header>
     </div>
   );
